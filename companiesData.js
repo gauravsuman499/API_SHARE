30fdd0d607c25184590984f92127b2604c654088
gauravsuman499 <116598523+gauravsuman499@users.noreply.github.com>
2024-05-31 22:47:40 +0530

    Add files via upload

---
+++ companiesData.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+// Define schema for the document
+const CompanySchema = new mongoose.Schema({
+    end_year: String,
+    intensity: Number,
+    sector: String,
+    topic: String,
+    insight: String,
+    url: String,
+    region: String,
+    start_year: String,
+    impact: String,
+    added: String,
+    published: String,
+    country: String,
+    relevance: Number,
+    pestle: String,
+    source: String,
+    title: String,
+    likelihood: Number
+});
+
+// Create model from the schema
+const companiesData = mongoose.model('companiesData', CompanySchema);
+
+module.exports = companiesData;
